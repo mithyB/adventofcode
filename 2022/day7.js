2f696aca486203e7693381e40cf12e87781d2523
Michael Buholzer <michael.buholzer@outlook.com>
2023-11-05 16:04:12 +0100

    Day 7 Part 1

---
+++ 2022/day7.js
@@ -0,0 +1,31 @@
+fetch('https://adventofcode.com/2022/day/7/input')
+    .then(r => r.text())
+    .then(t => t.split('\n'))
+    .then(t => {
+        var d = [];
+        var r = {};
+        t.forEach(l => {
+            if (l === '$ ls') {}
+            else if (l === '$ cd ..') {
+                d.pop();
+            }
+            else if (l.startsWith('$ cd')) {
+                d.push(l.substring(5));
+                r[d.join('/')] = 0
+            }
+            else if (l.startsWith('dir')) {}
+            else {
+                var s = parseInt(l.split(' ')[0]);
+                var p = [];
+                for (var dir of d) {
+                    p.push(dir);
+                    r[p.join('/')] += s;
+                }
+            }
+        });
+        return r;
+    })
+    .then(r => Object.values(r))
+    .then(r => r.filter(d => d <= 100_000))
+    .then(r => r.reduce((a, c) => a + c, 0))
+    .then(console.log)
