69a21f3dfe050f4bae83c6b33f4f3af31ac865db
Michael <michael.buholzer@outlook.com>
2023-11-06 14:43:53 +0100

    Update day8.js

--- 2022/day8.js
+++ 2022/day8.js
@@ -32,3 +32,35 @@ fetch('https://adventofcode.com/2022/day/8/input')
         return r.reduce((a, c) => c.filter(x => x === 'T').length + a, 0);
     })
     .then(console.log)
+
+fetch('https://adventofcode.com/2022/day/8/input')
+    .then(r => r.text())
+    .then(r => r.split('\n').map(l => l.split('').map(n => parseInt(n))))
+    .then(f => {
+        var r = [...Array(f.length)].map(e => Array(f[0].length).fill(0));
+
+        for (var y = 0; y < f.length; y++) {
+            for (var x = 0; x < f[0].length; x++) {
+                var height = f[y][x];
+                var maxNorth = false, distanceNorth = 0;
+                var maxEast = false, distanceEast = 0;
+                var maxSouth = false, distanceSouth = 0;
+                var maxWest = false, distanceWest = 0;
+
+                for (var i = 1; i < Math.max(f.length, f[0].length); i++) {                    
+                    if (!maxNorth && y-i >= 0) { distanceNorth++; maxNorth = f[y-i][x] >= height; } else { maxNorth = true }   
+                    if (!maxEast && x+i < f[0].length) { distanceEast++; maxEast = f[y][x+i] >= height; } else { maxEast = true }   
+                    if (!maxSouth && y+i < f.length) { distanceSouth++; maxSouth = f[y+i][x] >= height } else { maxSouth = true }   
+                    if (!maxWest && x-i >= 0) { distanceWest++; maxWest = f[y][x-i] >= height } else { maxWest = true }   
+                }
+
+                // r[y][x] = { value: distanceNorth * distanceEast * distanceSouth * distanceWest, distanceNorth, distanceEast, distanceSouth, distanceWest, x, y };
+                r[y][x] = distanceNorth * distanceEast * distanceSouth * distanceWest;
+            }
+        }
+
+        // return r;
+        
+        return Math.max(...r.map(t => Math.max(...t)))
+    })
+    .then(console.log)
