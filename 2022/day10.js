f950187d1aed1fb19cea89e2c25ae8f8fff51713
Michael <michael.buholzer@outlook.com>
2023-11-07 14:06:52 +0100

    Update day10.js

--- 2022/day10.js
+++ 2022/day10.js
@@ -19,3 +19,30 @@ fetch('https://adventofcode.com/2022/day/10/input')
     return o.reduce((a, c) => a + c, 0);
   })
   .then(console.log);
+
+fetch('https://adventofcode.com/2022/day/10/input')
+  .then(r => r.text())
+  .then(r => r.split('\n'))
+  .then(r => r.flatMap((l) => (l.startsWith('addx') ? ['.addx', l] : [l])))
+  .then(r => {
+    var x = 1;
+    var p = Array(240).fill('.');
+    r.forEach((l, i) => {
+      var c = i + 1;
+
+      if (l.startsWith('addx')) {
+        var v = parseInt(l.split(' ')[1]);
+        x += v;
+      }
+
+      p[i] = [x-1, x, x+1].includes(c % 40) ? '#' : '.';
+    });
+
+    var d = [];
+
+    while(p.length) d.push(p.splice(0,40));
+      
+    return d.splice(0, 6);
+  })
+  .then(d => d.map(l => l.join('')).join('\n'))
+  .then(console.log);
