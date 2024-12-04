7abba41b1bcc9eaed6c6e93232672b288b347cb4
Michael <michael.buholzer@outlook.com>
2024-12-04 08:05:39 +0100

    Create day04.js

---
+++ 2024/day04.js
@@ -0,0 +1,81 @@
+// Puzzle 1
+fetch('https://adventofcode.com/2024/day/4/input')
+    .then(x => x.text())
+    .then(x => x.split('\n'))
+    .then(x => x.slice(0, x.length - 1))
+    .then(input => {
+        const lineLength = input[0].length;
+        const search = 'XMAS';
+        let count = 0;
+
+        for (let y = 0; y < input.length; y++) {
+            for (let x = 0; x < lineLength; x++) {
+                if (input[y][x] === search[0]) {
+                    const isRight = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y]?.[x+i] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+                    const isLeft = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y]?.[x-i] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+                    const isUp = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y-i]?.[x] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+                    const isDown = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y+i]?.[x] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+                    
+                    const isUpRight = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y-i]?.[x+i] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+                    const isUpLeft = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y-i]?.[x-i] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+                    const isDownRight = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y+i]?.[x+i] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+                    const isDownLeft = (function() {
+                        for (let i = 1; i < search.length; i++) {
+                            if (input[y+i]?.[x-i] !== search[i]) return false;
+                        }
+                        return true;
+                    })()
+
+                    if (isRight) count++;
+                    if (isLeft) count++;
+                    if (isUp) count++;
+                    if (isDown) count++;
+                    if (isUpRight) count++;
+                    if (isUpLeft) count++;
+                    if (isDownRight) count++;
+                    if (isDownLeft) count++;
+                }
+                
+            }
+        }
+
+        return count;
+    })
+    .then(console.log)
+
+// Puzzle 2
